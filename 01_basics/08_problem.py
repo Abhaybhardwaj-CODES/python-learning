6dce091d37e11a2ea5c2e4b27fd23120ea6840c9
ABHAY BHARDWAJ <abhaybhardwaj3657@gmail.com>
2026-08-27 13:45:53 +0530

    Start Python problem solving journey

---
+++ 01_basics/08_problem.py
@@ -0,0 +1,4 @@
+Weight = float(input("Enter your weight in kilograms: "))
+Height = float(input("Enter your height in meters: "))
+BMI = Weight / (Height ** 2)
+print(f"Your Body Mass Index (BMI) is: {BMI:.2f}")
---
+++ 01_basics/09_problem.py
@@ -0,0 +1,5 @@
+Radius = int(input("Enter the radius of the circle: "))
+Area = 3.14 * (Radius ** 2)
+print(f"The area of the circle is: {Area}")
+Perimeter = 2 * 3.14 * Radius
+print(f"The perimeter of the circle is: {Perimeter}")
